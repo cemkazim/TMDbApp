e6c1fc47a884fa2eb1d286a4a2684152d7bcf8eb
Cem Kazım <cemkazim@hotmail.com>
2021-01-17 15:43:47 +0300

    storyboard is deleted from project

--- TMDbApp/AppDelegate.swift
+++ TMDbApp/AppDelegate.swift
@@ -11,10 +11,13 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.backgroundColor = UIColor.red
+        window?.rootViewController = UINavigationController(rootViewController: MovieListViewController())
+        window?.makeKeyAndVisible()
         return true
     }
 
